40160e9ba9e17c5046066d536574072ff7d6b9f4
Anderson de Souza Meneses <anderson.meneses@vector-unitech.com>
2022-11-12 00:02:38 -0300

    Added the Log Assembly

---
+++ src/Services/ChatRoomWithBot.Log/IoC/RegisterLogDependency.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatRoomWithBot.Log.IoC
+{
+    public static class RegisterLogDependency
+    {
+
+        public static IServiceCollection RegisterLogDependencies(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+
+
+
+            return services;
+        }
+
+    }
+}
