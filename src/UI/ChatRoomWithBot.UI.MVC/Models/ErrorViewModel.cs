ab51fb42e9e4f97a1032011aa0bbfe4a6a6143a8
Anderson Meneses <anderson.meneses@vector-unitech.com>
2022-11-12 21:09:38 -0300

    Adedd Views.

---
+++ src/UI/ChatRoomWithBot.UI.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatRoomWithBot.UI.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- src/UI/ChatRoomWithBot.UI.MVC/Program.cs
+++ src/UI/ChatRoomWithBot.UI.MVC/Program.cs
@@ -12,11 +12,11 @@ using MediatR;
 const string AspNetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
 
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); 
+
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 
 
 builder.Services
@@ -51,10 +51,20 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 //Configure RabbitMQ
 
